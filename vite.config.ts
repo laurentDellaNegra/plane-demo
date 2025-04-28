b412e7e9510c2b97d4c4f71ddd81189b4e93343d
Laurent <laurent@MacBook-Air-de-Laurent.local>
2025-04-28 23:34:26 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  publicDir: '../public/',
   plugins: [react()],
 })
