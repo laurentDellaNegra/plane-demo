b1c7bad3bf5dfee36801c424d0e91f14d4166679
Laurent Della-Negra <laurent.dellanegra@gmail.com>
2025-04-28 23:40:23 +0200

    fix import model

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  publicDir: '../public/',
+  publicDir: './public/',
   plugins: [react()],
 })
